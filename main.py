5c66f16bf63bc551e99c5b09ae38c65d27cbc3d1
FranciscoPerera <franciscopneto@gmail..com>
2025-10-14 22:03:45 -0300

    Criando o tarefas.py e criando uma nova rota

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from email import message
 from flask import Flask
+from tarefa import buscar_tarefas
 
 app = Flask(__name__)
 
@@ -8,6 +8,11 @@ def index():
     return{
         'message': 'Api Rodando!'
     }
+
+@app.route('/api/tarefas', methods=['GET'])
+def get_tarefas():
+    tarefas = buscar_tarefas()
+    return tarefas
 
 # Se for o modulo principal roda o projeto em debug
 if __name__ == '__main__':
---
+++ tarefa.py
@@ -0,0 +1,19 @@
+from flask import jsonify
+
+def buscar_tarefas():
+    tarefas = [
+        {
+            'id': 1,
+            'nome': 'Aprender digitação',
+            'descricao': 'Vamos aumentar o zoom para nao errar',
+            'status': 'Pendente'
+        },
+        {
+            'id': 2,
+            'nome': 'Aprender Python',
+            'descricao': 'Aprender Python para programar Api',
+            'status': 'Pendente'
+        }
+    ]
+    
+    return jsonify(tarefas)
